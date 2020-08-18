e847680c2bd78a607515f35027f16334c43607be
Hussein Elgridly <helgridly@foundationmedicine.com>
2020-08-18 17:45:26 -0400

    export some commands for use in code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const tools = require('./lib/tools');
+const commands = require('./lib/commands');
 
 module.exports = {
-  readFile: tools.readFile
+  readFile: tools.readFile,
+  readConfig: tools.readConfig,
+  performMigration: commands.performMigration,
+  migrationHistory: commands.migrationHistory,
+  listLocalMigrations: commands.listLocalMigrations
 };
